e4adbbb69ade950d7ab4ae5f691ce5d82e1ccdd6
MS_YSN <kaito.229@hotmail.co.jp>
2018-01-17 16:56:19 +0900

    2018 1/17

--- injection_program.ino
+++ injection_program.ino
@@ -1,9 +1,26 @@
+#include <>
+
+const int ac_mode 2;//digital,mode
+const int id_mode 3;//digital,mode
+const int ij_pin 5;//digital,injector
+const int pl_pin 0;//Analog,pulse generator
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(ac_mode,INPUT);
+  pinMode(id_mode,INPUT);
+  pinMode(ij_pin,OUTPUT);
+  pinMode(pl_pin,OUTPUT);
+
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(id_mode==HIGH)
+  {
+
+  }else{
+    
+  }
 
 }
